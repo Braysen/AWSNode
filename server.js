3c6935c9179d8a52b065d53b8a1c99b9fee22c0e
Braysen Torrejon Cerna <braysen.torrejon@ECOPLAZA.ECO>
2021-10-20 15:50:16 -0500

    Test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.send("Hello world with express!"));
+app.get('/', (req,res) => res.send("Hello world with express clone!"));
 
 app.listen(3000);
 console.log("Server on port 3000");
